b9ef41288f07cbef9b09c31c090a27dcf77b7a59
Sergio <65286@iessierradeguara.com>
2020-11-17 17:27:58 +0100

    Añadir Lista JS

--- JS-listaTareas/app.js
+++ JS-listaTareas/app.js
@@ -1,3 +1,33 @@
-let listaTareas = document.getElementById("listaTareas");
 
-listaTareas.innerHTML = listaTareas.innerHTML+"<li>Hola</li>"
+function anyadirTarea() {
+
+    //Tomo el valor de input
+
+    let eTarea = document.getElementById("idTarea");
+
+    //Compruebo y si tiene algo de texto
+
+    if (eTarea.value != "") {
+
+    //Si tiene texto, añado el elemento en la lista
+
+        anyadirElemento(eTarea.value);
+
+    //Despues de añadir borro el elemento input
+
+        eTarea.value = "";
+    }
+    //Si no tiene texto, no hago nada
+}
+
+//Funcion añadir elemento a la lista
+
+function anyadirElemento(textoTarea) {
+
+    let listaTareas = document.getElementById("listaTareas");
+    
+//Cogera un parametro y lo añadira a la lista listaTareas
+
+    listaTareas.innerHTML = listaTareas.innerHTML+"<li>"+textoTarea+"</li>";
+
+}
